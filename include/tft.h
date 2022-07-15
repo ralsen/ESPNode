648d828b9d1722388bb0c8ba049c313e0ccc84a0
ralsen <follrichs@gmx.de>
2022-07-15 18:32:51 +0200

    new version

---
+++ include/tft.h
@@ -0,0 +1,37 @@
+/*
+  tft.h
+
+  !!! for history see settings.h !!!
+
+  ToDos:    -
+
+  hints:    -
+*/
+
+#if (H_TFT_18 == H_TRUE)
+# ifndef _TFT_H_
+# define _TFT_H_
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#define TFT_CS        4
+#define TFT_RST       5 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC        2
+// initialize ST7735 TFT library with hardware SPI module
+//SCK (CLK) ---> NodeMCU pin D5 (GPIO14)
+//MOSI(DIN) ---> NodeMCU pin D7 (GPIO13)
+// BLK an 3,3V VCC  
+
+void tft_initR(int color);
+void tft_fillScreen(int color);
+void tft_setCursor(int x, int y);
+void tft_setTextColor(int color);
+void tft_setTextWrap(int mode);
+void tft_println(String str);
+void tft_print(String str);
+
+extern Adafruit_ST7735 tft;
+
+# endif // _TFT_H_
+#endif //(H_TFT == H_TRUE)
