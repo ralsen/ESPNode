147c1e522e35440f582777c9c8135309bc595f8d
ralsen <follrichs@gmx.de>
2022-07-24 19:20:49 +0200

    changes for HTML reading from files

--- include/WebServer.h
+++ include/WebServer.h
@@ -20,35 +20,13 @@
 #define W_TEXT_PLAIN      "text/plain"
 #define W_TEXT_HTML       "text/html"
 
-#define W_URL_ROOT        "/"
-#define W_URL_ON          "on"
-#define W_URL_OFF         "off"
-#define W_URL_FIX_IP      "fixip"
-#define W_URL_SCAN        "scan"
-#define W_URL_LED         "led"
-#define W_URL_INFO        "info"
-#define W_URL_RESET       "reset"
-#define W_URL_APMODE      "ap-mode"
-#define W_URL_STAMODE     "sta-mode"
-#define W_URL_HOSTNAME    "hostname"
-#define W_URL_STATUS      "status"
-#define W_URL_DEFAULT     "default"
-#define W_URL_ERASE       "erase"
-#define W_URL_SERVER      "server"
-#define W_URL_SERVICE     "service"
-#define W_URL_NETWORK     "network"
-#define W_URL_CONFIG      "config"
-#define W_URL_SHOWLOG     "showlog"
-#define W_URL_SHOWDIR     "showdir"
-#define W_URL_MEASCYC     "meascyc"
-#define W_URL_TRANSCYC    "transcyc"
-#define W_URL_PAGERELOAD  "pagereload"
-
 extern ESP8266WebServer server;
 
+void initHTML(void);
 void WebServerStart(void);
 void handleStatus();
 String BuildURL (void);
+String readHTML (String);
 
 #if (H_DS1820 == H_TRUE)
   String      BuildDS1820URL (void);
