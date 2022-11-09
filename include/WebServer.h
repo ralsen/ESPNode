42d3a41b19a0d86e7611e87f38038eb7d00c4f27
ralsen <follrichs@gmx.de>
2022-11-09 17:21:30 +0100

    some old prototypes removed

--- include/WebServer.h
+++ include/WebServer.h
@@ -22,8 +22,6 @@
 #define W_TEXT_PLAIN      "text/plain"
 #define W_TEXT_HTML       "text/html"
 
-//FOL extern ESP8266WebServer server;
-
 void initHTML(void);
 void WebServerStart(void);
 void handleStatus(void);
@@ -49,29 +47,21 @@ void startWebServer(void);
 void buildInfoPage(void);
 String buildConfPage(String);
 String buildNetworkPage(String);
-String BuildURL (void);
 String readHTML (String);
 String buildAppPage(String);
 String buildMainPage(String);
-String buildURL (void);
+String buildDict (void);
 String buildDS1820Page(void);
-String buildDS1820URL (void);
+String buildDS1820Dict (void);
 String buildSwitchPage(void);
-String buildSwitchURL(void);
+String buildSwitchDict(void);
 
 int checkInput(String, String);
 bool string_isNumber(String);
 
-#if (H_DS1820 == H_TRUE)
-  String      BuildDS1820URL (void);
-  String      BuildDS1820HTML(void);
-#elif (H_SWITCH == H_TRUE)
-  String      BuildSwitchURL(void);
-#endif
-
 #if (H_TOF == H_TRUE)
 String buildToFPage(void);
-String buildToFURL (void);
+String buildToFDict (void);
 #endif
 
 #endif // _WEBSERVER_H_
